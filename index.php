80c24a899609b3a17a5b95108a2f494451fb5121
Дмитрий Кречетников <dkrech07@gmail.com>
2021-06-03 22:17:57 +0500

    Выводит на карту точку по адресу

--- index.php
+++ index.php
@@ -1,13 +1,11 @@
 <html>
-    <head>
-        <link rel="stylesheet" href="libs/dropzone.css">
-        <link rel="stylesheet" href="libs/dropzone.css">
-        <script src="libs/dropzone.js"></script>
 
-        <script src="https://api-maps.yandex.ru/2.1/?apikey=4f3c09de-626b-498a-bc29-cff656b39532&lang=ru_RU" type="text/javascript"></script>
-    </head>
-    <body>
-        <main>
+<head>
+    <script src="https://api-maps.yandex.ru/2.1/?apikey=4f3c09de-626b-498a-bc29-cff656b39532&lang=ru_RU" type="text/javascript"></script>
+</head>
+
+<body>
+    <main>
 
         <div id="map" style="width: 800px; height: 400px"></div>
 
@@ -16,81 +14,39 @@
             <input type="file" name="uploadfile">
             <input type="submit" value="Загрузить файл">
         </form>
-        </main>
-        <script type="text/javascript">
-            // Функция ymaps.ready() будет вызвана, когда
-            // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-            ymaps.ready(init);
-            function init(){
-                // Создание карты.
-                var myMap = new ymaps.Map("map", {
-                    // Координаты центра карты.
-                    // Порядок по умолчанию: «широта, долгота».
-                    // Чтобы не определять координаты центра карты вручную,
-                    // воспользуйтесь инструментом Определение координат.
-                    center: [55.76, 37.64],
-                    // Уровень масштабирования. Допустимые значения:
-                    // от 0 (весь мир) до 19.
-                    zoom: 7
-                });
+    </main>
+    <script>
+        ymaps.ready(init);
 
-                // Создаем геообъект с типом геометрии "Точка".
-        myGeoObject = new ymaps.GeoObject({
-            // Описание геометрии.
-            geometry: {
-                type: "Point",
-                coordinates: [55.8, 37.8]
-            },
-            // Свойства.
-            properties: {
-                // Контент метки.
-                iconContent: 'Я тащусь',
-                hintContent: 'Ну давай уже тащи'
-            }
-        }, {
-            // Опции.
-            // Иконка метки будет растягиваться под размер ее содержимого.
-            preset: 'islands#blackStretchyIcon',
-            // Метку можно перемещать.
-            draggable: true
-        }),
-        myPieChart = new ymaps.Placemark([
-            55.847, 37.6
-        ], {
-            // Данные для построения диаграммы.
-            data: [
-                {weight: 8, color: '#0E4779'},
-                {weight: 6, color: '#1E98FF'},
-                {weight: 4, color: '#82CDFF'}
-            ],
-            iconCaption: "Диаграмма"
-        }, {
-            // Зададим произвольный макет метки.
-            iconLayout: 'default#pieChart',
-            // Радиус диаграммы в пикселях.
-            iconPieChartRadius: 30,
-            // Радиус центральной части макета.
-            iconPieChartCoreRadius: 10,
-            // Стиль заливки центральной части.
-            iconPieChartCoreFillStyle: '#ffffff',
-            // Cтиль линий-разделителей секторов и внешней обводки диаграммы.
-            iconPieChartStrokeStyle: '#ffffff',
-            // Ширина линий-разделителей секторов и внешней обводки диаграммы.
-            iconPieChartStrokeWidth: 3,
-            // Максимальная ширина подписи метки.
-            iconPieChartCaptionMaxWidth: 200
-        });
+        function init() {
+            var myMap = new ymaps.Map("map", {
+                // Координаты центра карты.
+                // Порядок по умолчанию: «широта, долгота».
+                // Чтобы не определять координаты центра карты вручную,
+                // воспользуйтесь инструментом Определение координат.
+                center: [55.76, 37.64],
+                // Уровень масштабирования. Допустимые значения:
+                // от 0 (весь мир) до 19.
+                zoom: 7
+            });
 
-    myMap.geoObjects
-        .add(new ymaps.Placemark([55.833436, 37.715175], {
-            balloonContent: '<strong>серобуромалиновый</strong> цвет'
-        }, {
-            preset: 'islands#dotIcon',
-            iconColor: '#735184'
-        }))
-            }
-        </script>
-    </body>
+            ymaps.geoQuery(ymaps.geocode('Екатеринбург, Решетникова, 4')).addToMap(myMap);
+
+            // var myGeocoder = ymaps.geocode("Екатеринбург");
+            // console.log(myGeocoder);
+            // myGeocoder.then(
+            //     function(res) {
+            //         // Выведем в консоль данные, полученные в результате геокодирования объекта.
+            //         console.log('Все данные геообъекта: ', res.geoObjects.get(0).properties.getAll());
+            //     },
+            //     function(err) {
+            //         // Обработка ошибки.
+            //     }
+            // );
+        }
+    </script>
+</body>
+
 </html>
 
 <?php
@@ -188,11 +144,11 @@ $connection->set_charset("utf8");
 // Загружаем файл Excel
 $PHPExcel_file = PHPExcel_IOFactory::load($file_url);
 
-// Преобразуем первый лист Excel в таблицу MySQL
-$PHPExcel_file->setActiveSheetIndex(0);
-echo excel2mysql($PHPExcel_file->getActiveSheet(), $connection, "excel2mysql0", 1) ? "OK\n" : "FAIL\n";
+// // Преобразуем первый лист Excel в таблицу MySQL
+// $PHPExcel_file->setActiveSheetIndex(0);
+// echo excel2mysql($PHPExcel_file->getActiveSheet(), $connection, "excel2mysql0", 1) ? "OK\n" : "FAIL\n";
 
 // Перебираем все листы Excel и преобразуем в таблицу MySQL
 foreach ($PHPExcel_file->getWorksheetIterator() as $index => $worksheet) {
-    echo excel2mysql($worksheet, $connection, "excel2mysql" . ($index != 0 ? $index : ""), 1) ? "OK\n" : "FAIL\n";
+    excel2mysql($worksheet, $connection, "excel2mysql" . ($index != 0 ? $index : ""), 1);
 }
